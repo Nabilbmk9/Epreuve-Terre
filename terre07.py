ef9c0c2bd7c7b73c4341e99e455b941345f78330
Nabilbmk9 <nabil.bmk9@gmail.com>
2022-01-08 12:29:51 +0100

    version final

--- terre07.py
+++ terre07.py
@@ -1,11 +1,17 @@
 import sys
 
+argument = [sys.argv]
 phrase = sys.argv[1]
 phrase_to_list = list(phrase)
 total =0
 
-for i in phrase_to_list : 
-    total+=1
+if len(argument)!= 1 or phrase.isnumeric():
+    print("erreur.")
+    sys.exit()
+
+else:
+    for i in phrase_to_list : 
+        total+=1
 
 print(total)
 
