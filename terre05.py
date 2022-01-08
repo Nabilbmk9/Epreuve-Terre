51a195f061801116d8e5e6be3110c42481804ba7
Nabilbmk9 <nabil.bmk9@gmail.com>
2022-01-08 09:20:24 +0100

    version final

---
+++ terre05.py
@@ -0,0 +1,18 @@
+import sys
+
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+
+if arg2 == "0" :
+    print("erreur.")
+    sys.exit()
+
+if int(arg2) > int(arg1) :
+    print("erreur.")
+    sys.exit()
+
+else : 
+    resultat = int(arg1) // int(arg2)
+    reste = resultat%2
+    print(f"Résultat : {int(resultat)}")
+    print(f"Reste : {int(reste)}")
